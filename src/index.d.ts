1e82f4db36d0711fb7f78c6a81275cfa48077a52
codelzn <lzn724958732@gmail.com>
2022-11-05 20:50:12 +0900

    learn-shaderMaterial

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,30 +1,3 @@
-import type { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-
-declare module '*.vert' {
-  const content: string;
-  export default content;
-}
-
-declare module '*.frag' {
-  const content: string;
-  export default content;
-}
-
-declare module '*.glsl' {
-  const content: string;
-  export default content;
-}
-
-declare module '*.vs' {
-  const content: string;
-  export default content;
-}
-
-declare module '*.fs' {
-  const content: string;
-  export default content;
-}
-
 declare module '*.glb' {
   const value: string;
   export default value;
--- src/pages/r3f/sketch05.tsx
+++ src/pages/r3f/sketch05.tsx
@@ -1,24 +1,85 @@
-import { FC } from 'react';
+import { FC, useRef } from 'react';
 import * as THREE from 'three';
-import { Canvas, useFrame } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
-
+import { Canvas, extend, useFrame, ReactThreeFiber } from '@react-three/fiber';
+import {
+  OrbitControls,
+  useGLTF,
+  useTexture,
+  Center,
+  Sparkles,
+  shaderMaterial,
+} from '@react-three/drei';
+import vertexShader from '@/shaders/portal/vertex.glsl';
+import fragmentShader from '@/shaders/portal/fragment.glsl';
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      portalMaterial: ReactThreeFiber.Object3DNode<
+        THREE.ShaderMaterial,
+        typeof PortalMaterial
+      >;
+    }
+  }
+}
+const PortalMaterial = shaderMaterial(
+  {
+    uTime: 0,
+    uColorStart: new THREE.Color('#ffffff'),
+    uColorEnd: new THREE.Color('#000000'),
+  },
+  vertexShader,
+  fragmentShader
+);
+extend({ PortalMaterial });
 const Experience: FC = () => {
+  const { nodes } = useGLTF('/models/portal.glb');
+  const bakedTexture = useTexture('/models/baked.jpg');
+  const portalMaterial = useRef<any>();
+  useFrame((state, delta) => {
+    portalMaterial.current.uniforms.uTime.value += delta;
+  });
   return (
     <>
       <color args={['#201919']} attach="background" />
       <OrbitControls makeDefault />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <Center>
+        <mesh geometry={(nodes.baked as THREE.Mesh).geometry}>
+          <meshBasicMaterial map={bakedTexture} map-flipY={false} />
+        </mesh>
+        <mesh
+          geometry={(nodes.poleLightA as THREE.Mesh).geometry}
+          position={(nodes.poleLightA as THREE.Mesh).position}
+        >
+          <meshBasicMaterial color="#ffffe5" />
+        </mesh>
+        <mesh
+          geometry={(nodes.poleLightB as THREE.Mesh).geometry}
+          position={(nodes.poleLightB as THREE.Mesh).position}
+        >
+          <meshBasicMaterial color="#ffffe5" />
+        </mesh>
+        <mesh
+          geometry={(nodes.portalLight as THREE.Mesh).geometry}
+          position={(nodes.portalLight as THREE.Mesh).position}
+          rotation={(nodes.portalLight as THREE.Mesh).rotation}
+        >
+          <portalMaterial ref={portalMaterial} />
+        </mesh>
+        <Sparkles
+          size={6}
+          scale={[4, 2, 4]}
+          position-y={1}
+          speed={0.2}
+          count={40}
+        />
+      </Center>
     </>
   );
 };
 
 export default function Sketch05() {
   return (
-    <Canvas>
+    <Canvas flat>
       <Experience />
     </Canvas>
   );
